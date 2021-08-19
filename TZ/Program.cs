d31e913481f7b4d91362da4ff77e9eca32f5aad3
DenisPachekhon <denis.pachekhon.work@gmail.com>
2021-08-19 12:26:23 +0300

    Add project files.

---
+++ TZ/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+
+namespace TZ
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+       
+        public static async Task<string> GetFood(string linkoffood)
+        {
+
+            HttpResponseMessage response = await client.GetAsync(linkoffood);
+
+            HttpContent content = response.Content;
+
+            string result = await content.ReadAsStringAsync();
+
+            return result;
+
+        }
+
+        static async Task Main(string[] args)
+        {
+            bool exit = false;
+
+            while (!exit) 
+            {
+                Console.WriteLine($"Нажмите клавишу 'S' чтобы отправить запрос\nЧтобы выйти нажмите клавишу 'E'");
+                
+                ConsoleKeyInfo key = new ConsoleKeyInfo();
+                key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.E:
+                        {
+                            exit = true;
+                            Console.Clear();
+                            break;
+                        }
+                    case ConsoleKey.S:
+                        {
+                            Console.Clear();
+                            var json = await GetFood($"https://tester.consimple.pro/");
+
+                            var options = new JsonSerializerOptions
+                            {
+                                IncludeFields = true,
+                            };
+                            var twoList = JsonSerializer.Deserialize<TwoList>(json, options);
+
+                            Console.WriteLine(twoList);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Выбрана неверная клавиша");
+                            break;
+                        }
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ TZ/TwoList.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace TZ
+{
+    internal class TwoList
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+
+        public override string ToString()
+        {
+            var table = new ConsoleTable();
+
+            table.SetHeaders("Product name", "Category name");
+
+            foreach (var item in Products)
+            {
+                var categortyName = (from category in Categories
+                                    join product in Products
+                                    on category.Id equals product.CategoryId
+                                    where product.Name == item.Name 
+                                    && product.Id == item.Id 
+                                    select category.Name).First();
+
+                table.AddRow(item.Name, categortyName);
+            }
+
+            return table.ToString();
+        }
+    }
+}
